ea136619f232b612ea1376373a0f8720eeacffdb
Rod-yan <ryanis91@yahoo.com>
2019-10-16 22:38:18 -0300

    Corrijo error en server mapping, no cargaba index

--- src/main/java/com/yotereparo/configuration/AppInitializer.java
+++ src/main/java/com/yotereparo/configuration/AppInitializer.java
@@ -16,7 +16,7 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
   
     @Override
     protected String[] getServletMappings() {
-        return new String[] { "/*" };
+        return new String[] { "/" };
     }
  
 }
