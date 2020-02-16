28603fafc2531931a0ca1c79ddaff10a03bc9f46
Ignacio <warkanlock@gmail.com>
2020-02-15 22:06:49 -0300

    Rise of the navheader

--- yotereparo.js/src/About/About.jsx
+++ yotereparo.js/src/About/About.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import CardContent from "../Container/CardContent";
+import ElementContainer from "../Container/ElementContainer";
 
 function About(props) {
   return (
-    <div className="centered">
+    <div className="">
       <div className="row">
         <div className="col-12">
-          <CardContent
-            cardIcon="fa-hands-helping"
-            cardDescription="About page of <strong>YoTeReparo.js</strong>"
-            cardUrl="/buscar"
-            cardButtonText="BUSCAR AYUDA"
-          />
+          <ElementContainer>aaaaaaaaaaaaaaaaaaaaaaaaaa</ElementContainer>
         </div>
       </div>
     </div>
