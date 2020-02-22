a27c156e47a0dab68e31611ab5195c9143818e64
Rodyan <Rodyan@192.168.0.7>
2020-02-22 11:48:10 -0300

    Art generator para logo print en tiempo de inicializacion

--- src/main/java/com/yotereparo/configuration/AppInitializer.java
+++ src/main/java/com/yotereparo/configuration/AppInitializer.java
@@ -1,8 +1,19 @@
 package com.yotereparo.configuration;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import com.yotereparo.util.ASCIIArtGenerator;
+import com.yotereparo.util.ASCIIArtGenerator.ASCIIArtFont;
+
+@Component
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	
+	@Autowired
+    private ASCIIArtGenerator artGen;
 	
     @Override
     protected Class<?>[] getRootConfigClasses() {
@@ -18,5 +29,14 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
     protected String[] getServletMappings() {
         return new String[] { "/" };
     }
+    
+    @PostConstruct
+    public void init() throws Exception {
+    	String title = getClass().getPackage().getImplementationTitle();
+    	String version = getClass().getPackage().getImplementationVersion();
+		System.out.println("===========================================================");
+		artGen.printTextArt(title, 10, ASCIIArtFont.ART_FONT_DIALOG, "x");
+		System.out.println("============================================= Version "+version);
+	}
  
 }
