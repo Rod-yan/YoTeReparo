96fa6b1b5bbfbd02d63461b0935822e708b4bd67
Gabriel Capitanelli <gabrielcapitanelli@hotmail.com>
2020-02-23 00:49:46 -0300

    Creación de Clase Requerimiento

---
+++ src/main/java/com/yotereparo/model/Requirement.java
@@ -0,0 +1,78 @@
+package com.yotereparo.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="requerimiento")
+public class Requirement {
+	
+	@Id
+	@NotNull(message = "{Requirement.id.not.null}")
+	@Column(name = "id_requerimiento", nullable = false)
+	private String id;
+	
+	@NotEmpty(message = "{Requirement.descripcion.not.empty}")
+	private String descripcion;
+	
+	public Requirement() { }
+
+	/* Getters & Setters */
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+		
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((descripcion == null) ? 0 : descripcion.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Requirement other = (Requirement) obj;
+		if (descripcion == null) {
+			if (other.descripcion != null)
+				return false;
+		} else if (!descripcion.equals(other.descripcion))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Requirement [id=" + id + ", descripcion=" + descripcion +"]";
+	}
+
+}
