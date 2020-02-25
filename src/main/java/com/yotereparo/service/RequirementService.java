1b6b9489b4d9147437bdb69076200ec08c21ba71
Gabriel Capitanelli <gabrielcapitanelli@hotmail.com>
2020-02-25 17:42:05 -0300

    Creacion Service de Requerimiento

---
+++ src/main/java/com/yotereparo/service/RequirementService.java
@@ -0,0 +1,19 @@
+package com.yotereparo.service;
+
+import java.util.List;
+
+import com.yotereparo.model.Requirement;
+
+public interface RequirementService {
+	
+	 List<Requirement> getAllRequirements(); 
+     
+	 Requirement getRequirementById(Integer id);
+	    
+	 Requirement getRequirementByDescription(String description);
+	    
+	    boolean exist(Integer id);
+	    
+	    boolean exist(String description);
+
+}
---
+++ src/main/java/com/yotereparo/service/RequirementServiceImpl.java
@@ -0,0 +1,58 @@
+package com.yotereparo.service;
+
+import java.util.List;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.yotereparo.dao.RequirementDaoImpl;
+import com.yotereparo.model.Requirement;
+
+/**
+ * Capa de servicio para Requerimientos.
+ * El objetivo de la misma es servir de interfaz entre el modelo y la capa de acceso a datos,
+ * expone métodos para uso público en el contexto de la aplicación.
+ * 
+ * Implementa lógica de negocio donde correspondiera.
+ * 
+ * @author Gabriel Capitanelli
+ * 
+ */
+
+@Service("requirementService")
+@Transactional 
+public class RequirementServiceImpl implements RequirementService {
+	
+	private static final Logger logger = LogManager.getLogger(RequirementServiceImpl.class);
+	
+	@Autowired
+	private RequirementDaoImpl dao;
+
+	public List<Requirement> getAllRequirements() {
+		logger.debug("Fetching all requirements");
+		return dao.getAllRequirements();
+	}
+
+	public Requirement getRequirementById(Integer id) {
+		logger.debug(String.format("Fetching requirement by id <%s>", id));
+		return dao.getRequirementById(id);
+	}
+	
+	public Requirement getRequirementByDescription(String description) {
+		logger.debug(String.format("Fetching requirement <%s>", description));
+		return dao.getRequirementByDescription(description);
+	}
+	
+	public boolean exist(Integer id) {
+		logger.debug(String.format("Verifying existence of requirement with id <%s>", id));
+		return (getRequirementById(id) != null);
+	}
+	
+	public boolean exist(String description) {
+		logger.debug(String.format("Verifying existence of requirement <%s>", description));
+		return (getRequirementByDescription(description) != null);
+	}
+}
