63a7e61e779a379fc88e836b55e801cff15ecb24
Rod-yan <ryanis91@yahoo.com>
2020-06-20 20:09:54 -0300

    Rem. imports no utilizados

--- src/main/java/com/yotereparo/controller/AuthController.java
+++ src/main/java/com/yotereparo/controller/AuthController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.ResponseEntity.HeadersBuilder;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
